945b91819f7ab746455e78a86d4caf8a00c2bf6e
fabianomcs <fabianomcs@users.noreply.github.com>
2021-01-24 12:17:37 -0300

    crido atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,29 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=34):
+    def __init__(self, nome=None, idade=34, *filhos):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Maia')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome ='Fabiano'
-    print(p.nome)
-    print(p.idade)
+
+    jose = Pessoa(nome='Jose', idade=2)
+    maia = Pessoa('Maia', 30, jose)
+    fabiano = Pessoa(nome='Fabiano', idade=5)
+    maia = Pessoa('Maia', 30, fabiano)
+
+    print(Pessoa.cumprimentar(maia))
+    print(id(maia.nome))
+    print('1', maia.cumprimentar())
+    print(maia.nome, maia.idade)
+    print(maia.idade)
+    print(str(maia.filhos.count))
+
+    for filho in maia.filhos:
+        print(Pessoa.cumprimentar(filho.nome))
+
+    for filho in maia.filhos:
+        print(filho.nome)
